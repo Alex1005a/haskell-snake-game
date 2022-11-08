ffc06973fb185c13fc843597a2f1f811bf743bec
Alexander <azazf93@gmail.com>
2022-11-08 23:09:12 +0300

    Remove sample code

--- terminal/Main.hs
+++ terminal/Main.hs
@@ -17,45 +17,37 @@ main :: IO ()
 main = do
       gen <- newStdGen
       let snake' = Snake (fromList [(0, 0)]) Right
-      let (gameState, gen') = runState (initGameState snake' mh mw) gen
+      let (gameState, gen') = runState (initGameState snake' (mh-3) (mw-3)) gen
       sizeCheck
       errorPress $ playGame $ aloneInARoom gameState gen'
       where
-          (mh, mw) = snd boundaries
+          (mh, mw) = boundaries
 
 
 -- game specification
 aloneInARoom :: GameState -> StdGen -> Game MyState
-aloneInARoom gameState gen = Game 13                       -- ticks per second
-                    (MyState (10, 10) gameState gen
-                             Stop False)     -- init state
-                    (\_ s e -> logicFun s e) -- logic function
+aloneInARoom gameState gen = Game 9                  
+                    (MyState gameState gen False)  
+                    (\_ s e -> logicFun s e) 
                     (\r s -> centerFull r $
-                               drawFun s)    -- draw function
-                    gsQuit                   -- quit function
+                               drawFun s)    
+                    gsQuit                   
 
 sizeCheck :: IO ()
-sizeCheck = let (w, h) = T.swap . snd $ boundaries
+sizeCheck = let (w, h) = T.swap  boundaries
             in assertTermDims w h
 
--------------------------------------------------------------------------------
--- Types
 
-data MyState = MyState { gsCoord :: Coords,
-                         gsGameState :: GameState,
+
+data MyState = MyState { gsGameState :: GameState,
                          stdGen :: StdGen,
-                         gsMove  :: Move,
                          gsQuit  :: Bool }
              deriving (Show)
 
-data Move = N | S | E | W | Stop
-          deriving (Show, Eq)
+boundaries :: Coords
+boundaries = (20, 40)
 
-boundaries :: (Coords, Coords)
-boundaries = ((1, 1), (20, 40))
 
--------------------------------------------------------------------------------
--- Logic
 
 logicFun :: MyState -> Event -> MyState
 logicFun gs (KeyPress 'q') = gs { gsQuit = True }
@@ -82,7 +74,7 @@ changeDirection _ = Nothing
 -- Draw
 
 swapAdd1 ::  (Int, Int) -> (Int, Int)
-swapAdd1 (x, y) = (y + 1, x + 1)
+swapAdd1 (x, y) = (y + 2, x + 2)
 
 drawCoord :: Char -> Coord -> Draw
 drawCoord ch coord = coord % cell ch
@@ -92,13 +84,13 @@ drawSnake snake' plane = do
       foldr (\a b -> b & drawCoord '*' a) plane $ swapAdd1 <$> snake' ^. parts
 
 drawFun :: MyState -> Plane
-drawFun (MyState _ gameState _ _ _) =
+drawFun (MyState gameState _ _) =
             blankPlane mw     mh            &
-                (1, 1)   % box mw mh '.'                   &
+                (1, 1)   % box mw mh '#'                   &
                 (2, 2)   % box (mw-2) (mh-2) ' '           &
                 drawSnake (gameState ^. snake)             &
                 drawCoord 'A' (swapAdd1 (gameState ^. foodCoord)) 
     where
           mh :: Height
           mw :: Width
-          (mh, mw) = snd boundaries
+          (mh, mw) = boundaries
